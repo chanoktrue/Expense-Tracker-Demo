af9e73c135e286f7af8ca73b52a5c11caa7d04ef
chanoktrue <chanoktrue@me.com>
2023-12-28 08:46:52 +0700

    Initial Commit

---
+++ Expense Tracker Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Tracker Demo
+//
+//  Created by Thongchai Subsaidee on 28/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Tracker Demo/Expense_Tracker_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Tracker_DemoApp.swift
+//  Expense Tracker Demo
+//
+//  Created by Thongchai Subsaidee on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Tracker_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
